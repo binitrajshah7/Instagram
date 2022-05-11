9f3ae871eaf9eee1490ca9ed43fb474ebf0f5d9a
Binit Kumar Shah <binitrajshah931@gmail.com>
2022-05-11 17:56:48 +0530

    1.03 ==> Firebase SignIn & Firebase SignOut

--- app/src/main/java/com/gmail/binitrajshah931/AccountSettingsActivity.kt
+++ app/src/main/java/com/gmail/binitrajshah931/AccountSettingsActivity.kt
@@ -1,11 +1,28 @@
 package com.gmail.binitrajshah931
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gmail.binitrajshah931.databinding.ActivityAccountSettingsBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class AccountSettingsActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityAccountSettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_settings)
+
+        binding = ActivityAccountSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logoutBtn.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/gmail/binitrajshah931/SignInActivity.kt
+++ app/src/main/java/com/gmail/binitrajshah931/SignInActivity.kt
@@ -1,10 +1,14 @@
 package com.gmail.binitrajshah931
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.gmail.binitrajshah931.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
+
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
@@ -16,6 +20,55 @@ class SignInActivity : AppCompatActivity() {
 
         binding.signupLinkBtn.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        binding.loginBtn.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        val email = binding.emailLogin.text.toString()
+        val password = binding.passwordLogin.text.toString()
+
+        when {
+            TextUtils.isEmpty(email) -> Toast.makeText(this,
+                "Email is Required",
+                Toast.LENGTH_SHORT).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this,
+                "Password is Required",
+                Toast.LENGTH_SHORT).show()
+            else -> {
+                val progressDialog = ProgressDialog(this)
+
+                progressDialog.apply {
+                    setTitle("SigningIn")
+                    setMessage("Please Wait")
+                    setCanceledOnTouchOutside(false)
+                    show()
+                }
+
+                val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        progressDialog.dismiss()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        // for not allowing users to go signIn or signup until logout is pressed
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        val message = task.exception.toString()
+                        Toast.makeText(this,
+                            "Error: $message",
+                            Toast.LENGTH_SHORT).show()
+                        FirebaseAuth.getInstance().signOut()
+                        progressDialog.dismiss()
+                    }
+                }
+            }
         }
     }
 
